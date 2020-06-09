46fde80dd98cfb4db227550ebf6f6e27e0dab5d8
Kayla Kingston <kkingston09@gmail.com>
2020-06-09 12:59:29 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,12 +4,12 @@
 
 
 def key_for_min_value(name_hash)
-      name_hash.reduce([nil, nil])
-      { |acc, (key, value)| 
-        if acc[0].nil? [key, value] 
-        elsif acc[1] > value [key, value] 
-        else acc 
-        end }.first 
+  name_hash.reduce([nil, nil])
+    { |acc, (key, value)| 
+      if acc[0].nil? [key, value] 
+      elsif acc[1] > value [key, value] 
+      else acc 
+      end }.first 
 end
 
 
