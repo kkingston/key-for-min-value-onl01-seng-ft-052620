03db72a8bb7ca3b15fbb72021f0cafe7c7b031f9
Kayla Kingston <kkingston09@gmail.com>
2020-06-09 12:35:28 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,18 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+ 
+   
 
 end
+
+# def key_for_min_value(name_hash)
+#       name_hash.reduce([nil, nil])
+#       { |acc, (key, value)| 
+#         if acc[0].nil? [key, value] 
+#         elsif acc[1] > value [key, value] 
+#         else acc 
+#         end }.first 
+# end
+
+
